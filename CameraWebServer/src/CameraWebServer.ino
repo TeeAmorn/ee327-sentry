0fa02255d7743cf3a79b4cb28c4f6f349331bd3c
jurgz42 <jurgentisza2024@u.northwestern.edu>
2022-04-14 13:16:07 -0500

    Correct Camera code

--- CameraWebServer/src/CameraWebServer.ino
+++ CameraWebServer/src/CameraWebServer.ino
@@ -8,19 +8,12 @@
 //
 
 // Select camera model
-#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
-//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
-//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
-//#define CAMERA_MODEL_AI_THINKER // Has PSRAM
-//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
+#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
 
 #include "camera_pins.h"
 
-const char* ssid = "*********";
-const char* password = "*********";
+const char* ssid = "*********"; //insert internet ID
+const char* password = "*********"; //insert password
 
 void startCameraServer();
 
